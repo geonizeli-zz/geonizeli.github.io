a8e56d19a71dc9da9b124d95fedbeab0d80d9fd3
João Victor <jvsouzax@gmail.com>
2020-03-09 17:42:26 -0300

    add: background on container and animation on gadgets

--- src/javascript/functions.js
+++ src/javascript/functions.js
@@ -1,7 +1,7 @@
 const displayCard = function(id) {
   element = document.getElementById(id)
   if (element.classList.value == 'card hidden') {
-    element.classList.value = 'card'
+    element.classList.value = 'card animated zoomIn'
   } else {
     element.classList.value = 'card hidden'
   }
